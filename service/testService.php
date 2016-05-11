7d4a451b631d8b522ea5bf182da506efa0ead675
Alex <narokwq@gmail.com>
2016-05-11 00:45:46 -0300

    Correção de bug

--- service/testService.php
+++ service/testService.php
@@ -4,12 +4,7 @@ include "Request.php";
 include "Consts.php";
 
 $content = new ServiceCurl();
-$request = new Request(Consts::END_POINT_MATRICULA, ["ra" => "1320110212", "token" => Consts::TOKEN_MATRICULA]);
-
-
-$endPoint = "http://sistemas.unipe.br:8080/uniservicetoten/totenAPI/consultaSala/v1";
-$endPoint1 = "http://sistemas.unipe.br:8080/uniservice/cursoAPI/consultaDisciplinasCurso/v1"; // 'c' => '003', 'p'=>'1', 'token'=>'unipe@2016', 'pr'=>'123123'
-
+$request = new Request(Consts::END_POINT_MATRICULA, ["ra" => "1320120212", "token" => Consts::TOKEN_MATRICULA]);
 
 ?>
 <html>
